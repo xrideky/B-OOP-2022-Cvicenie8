c0d8f8421489f2bbfa24ec9af6d8a0acf946ee80
xrideky <xrideky@stuba.sk>
2022-04-18 19:17:01 +0200

    labeled for a break

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,40 +1,18 @@
 package sk.stuba.fei.uim.oop;
 
-import javax.swing.*;
-import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-            JFrame frame = new JFrame();
-            frame.setSize(500,500);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.getContentPane().setBackground(Color.cyan);
-            frame.setLayout(new BorderLayout());
-
-            Logic logic = new Logic(frame);
-            frame.addKeyListener(logic);
-
-            JPanel menu = new JPanel();
-            menu.setBackground(Color.yellow);
-            menu.setLayout(new GridLayout(2,1));
-
-            JSlider slider = new JSlider(JSlider.HORIZONTAL,10,50,20);
-            slider.setMajorTickSpacing(5);
-            slider.setMinorTickSpacing(5);
-            slider.setSnapToTicks(true);
-            slider.setPaintTicks(true);
-            slider.setPaintLabels(true);
-            slider.addChangeListener(logic);
-
-            //aby nam fungoval KeyListener
-            slider.setFocusable(false);
-
-            menu.add(slider);
-            menu.add(logic.getLabel());
-
-            frame.add(menu,BorderLayout.PAGE_END);
-
-
-            frame.setVisible(true);
+        vonkajsiFor:
+            for(int i=1;i<5;i++){
+                    for(int j=10;j>0;j--){
+                            System.out.println("---");
+                            if(i%3==0){
+                                    break vonkajsiFor;
+                            }
+                            System.out.println(i + ", " + j);
+                    }
+            }
+            System.out.println("koniec");
     }
 }
